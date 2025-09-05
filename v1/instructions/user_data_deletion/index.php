aa068639b1ebb55469d9677e4c64bd0d5c6b97ee
Evangelos <vaggdedes@gmail.com>
2025-09-06 00:30:54 +0300

    added instructions

---
+++ v1/instructions/user_data_deletion/index.php
@@ -0,0 +1,13 @@
+<?php
+require '/var/www/.structure/library/base/utilities.php';
+header('Content-type: text/plain');
+echo "USER DATA DELETION\n";
+echo "Effective Date: 6th of September 2025\n\n";
+echo "Please email us and attach any relevant details such as email address, usernames, full names, e.t.c you have used with our services.\n";
+echo "We will find any correlated information and process your request as soon as possible, confirming to you once the deletion has been completed.\n\n";
+
+echo "CONTACT US\n";
+echo "If you have questions about this process, you can contact us at:\n";
+echo "Idealistic\n";
+echo "contact@idealistic.ai\n";
+echo "Järvevana tee 9, Tallinn, 11314, Estonia";
